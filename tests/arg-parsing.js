31a0adae6780f54e1bcb7b316c6c8881c1f39edc
Evgeniy Goryuchkin <goryuchkin@gmail.com>
2024-10-14 13:04:17 +0300

    switch to github actions, fix some tests

--- tests/arg-parsing.js
+++ tests/arg-parsing.js
@@ -1,9 +1,6 @@
 /* eslint-env mocha */
-
-const Promise = require('bluebird')
-const request = Promise.promisify(require('request'))
 const fs = require('fs')
-
+const request = require('./request')
 const todos = require('./app/features/todos')
 const users = require('./app/features/users')
 
@@ -13,64 +10,68 @@ describe('buhoi arg parsing', function () {
   })
 
   it('should treat no content as empty arg array', async function () {
-    const { statusCode } = await request({
+    const { status } = await request({
       url: 'https://localhost:3001/rpc/todos.publicProcedure',
-      method: 'POST',
-      headers: { 'content-type': 'application/json' },
-      strictSSL: false,
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
       timeout: 1000,
+      validateStatus: () => true,
     })
     todos.publicProcedureSpy.calledOnce.should.eql(true)
     todos.publicProcedureSpy.lastCall.args.slice(0, -2).should.eql([])
-    statusCode.should.eql(200)
+    status.should.eql(200)
   })
 
-  it('should treat no args in qs as empty arg array', async function () {
-    const { statusCode } = await request({
+  it('should treat no args in params as empty arg array', async function () {
+    const { status } = await request({
       url: 'https://localhost:3001/rpc/todos.publicProcedure',
-      method: 'GET',
-      strictSSL: false,
+      method: 'get',
+      headers: { 'Content-Type': 'application/json' },
       timeout: 1000,
+      validateStatus: () => true,
     })
     todos.publicProcedureSpy.calledOnce.should.eql(true)
     todos.publicProcedureSpy.lastCall.args.slice(0, -2).should.eql([])
-    statusCode.should.eql(200)
+    status.should.eql(200)
   })
 
   it('should respond with 500 if args are invalid JSON', async function () {
-    const { statusCode } = await request({
+    const { status } = await request({
       url: 'https://localhost:3001/rpc/todos.publicProcedure',
-      method: 'GET',
-      qs: { args: 'arg' },
-      strictSSL: false,
+      method: 'get',
+      headers: { 'Content-Type': 'application/json' },
+      params: { args: 'arg' },
       timeout: 1000,
+      validateStatus: () => true,
     })
-    statusCode.should.eql(500)
+    status.should.eql(500)
   })
 
   it('should respond with 500 if body does not contains array of args', async function () {
-    const { statusCode } = await request({
+    const { status } = await request({
       url: 'https://localhost:3001/rpc/todos.publicProcedure',
-      method: 'POST',
-      json: 'dolphin',
-      strictSSL: false,
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      data: 'dolphin',
       timeout: 1000,
+      validateStatus: () => true,
     })
-    statusCode.should.eql(500)
+    status.should.eql(500)
   })
 
   it('should receive forms with files', async function () {
-    const { statusCode } = await request({
+    const form = new FormData()
+    form.append('user_id', 10)
+    form.append('avatar', new Blob(await fs.createReadStream(`${__dirname}/app/public/index.html`).toArray()))
+
+    const { status } = await request({
       url: 'https://localhost:3001/rpc/users.uploadAvatar',
-      method: 'POST',
-      formData: {
-        user_id: 10,
-        avatar: fs.createReadStream(`${__dirname}/app/public/index.html`),
-      },
-      strictSSL: false,
+      method: 'post',
+      data: form,
       timeout: 1000,
+      validateStatus: () => true,
     })
-    statusCode.should.eql(200)
+    status.should.eql(200)
     users.uploadAvatarSpy.calledOnce.should.eql(true)
     const args = users.uploadAvatarSpy.lastCall.args
     args[0].should.have.property('user_id')
--- tests/mocha-hooks.js
+++ tests/mocha-hooks.js
@@ -2,12 +2,17 @@
 
 const buhoi = require('../src')
 
-before(() => buhoi.start(buhoi.config.simple({
-  featuresPath: `${__dirname}/app/features`,
-  publicPath: `${__dirname}/app/public`,
-  webpackConfigPath: `${__dirname}/app/pages/webpack.config.js`,
-  isAuthorized: (session, feature, _procedure) =>
-    feature !== 'secrets' || (session && session.startsWith('dodo')),
-})))
-
-after(() => buhoi.stop())
+exports.mochaHooks = {
+  async beforeAll () {
+    await buhoi.start(buhoi.config.simple({
+      featuresPath: `${__dirname}/app/features`,
+      publicPath: `${__dirname}/app/public`,
+      webpackConfigPath: `${__dirname}/app/pages/webpack.config.js`,
+      isAuthorized: (session, feature, _procedure) =>
+        feature !== 'secrets' || (session && session.startsWith('dodo')),
+    }))
+  },
+  async afterAll () {
+    await buhoi.stop()
+  },
+}
